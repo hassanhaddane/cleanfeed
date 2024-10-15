d84c91c31c9a1b0014d76c3bec7d8d8cb34994f6
Fabien-Jrt <67684689+Fabien-jrt@users.noreply.github.com>
2024-10-15 10:52:40 +0200

    Format landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -11,40 +11,49 @@ export const LandingPage = () => {
   };
 
   return (
-      <div className="landing-page">
-        <header className="header">
-          <img className="cleanfeed-logo" alt="CleanFeed Logo" src={cleanfeedLogo} />
-          <div className="nav-links">
-            <span className="about">À propos</span>
-            <span className="contact">Nous contacter</span>
-            <div className="language-selector">
-              <span className="language">FR</span>
-              <span className="globe-icon">M</span>
-            </div>
+    <div className="landing-page">
+      <header className="header">
+        <img
+          className="cleanfeed-logo"
+          alt="CleanFeed Logo"
+          src={cleanfeedLogo}
+        />
+        <div className="nav-links">
+          <span className="about">À propos</span>
+          <span className="contact">Nous contacter</span>
+          <div className="language-selector">
+            <span className="language">FR</span>
+            <span className="globe-icon">M</span>
           </div>
-        </header>
+        </div>
+      </header>
 
-        <main className="">
-          <div className="flex-col">
-            <div className="flex items-baseline">
-          <img className="" alt="CleanFeed Logo" src={cleanfeedLogo} />
+      <main className="">
+        <div className="flex-col">
+          <div className="flex items-baseline">
+            <img className="" alt="CleanFeed Logo" src={cleanfeedLogo} />
             <h1 className="text-3xl font-bold">
-              Protégez-vous en ligne<br />
-              avec CleanFeed : filtrez,<br />
+              Protégez-vous en ligne
+              <br />
+              avec CleanFeed : filtrez,
+              <br />
               signalez, et respirez.
             </h1>
-            </div>
           </div>
-          <button className="flex gap-2 items-baseline px-8 py-4 bg-blue-500 text-white rounded-full" onClick={handleLoginClick}>
-            Connexion <span>→</span>
-          </button>
-        </main>
+        </div>
+        <button
+          className="flex gap-2 items-baseline px-8 py-4 bg-blue-500 text-white rounded-full"
+          onClick={handleLoginClick}
+        >
+          Connexion <span>→</span>
+        </button>
+      </main>
 
-        <footer className="footer">
-          <div className="footer-links">
-            <span>Conditions d'utilisation | Confidentialité</span>
-          </div>
-        </footer>
-      </div>
+      <footer className="footer">
+        <div className="footer-links">
+          <span>Conditions d'utilisation | Confidentialité</span>
+        </div>
+      </footer>
+    </div>
   );
 };
